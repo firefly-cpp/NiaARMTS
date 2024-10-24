fcc590466ff6c3dd22e32d1789f70d3bf52f59df
Iztok Fister Jr. <iztok@iztok.dev>
2024-10-24 11:35:37 +0200

    Bump version to 0.1.1

--- niaarmts/__init__.py
+++ niaarmts/__init__.py
@@ -6,4 +6,4 @@ from niaarmts.metrics import calculate_support, calculate_confidence, calculate_
 
 __all__ = ["Dataset", "Feature", "build_rule", "NiaARMTS", "calculate_support", "calculate_confidence", "calculate_inclusion_metric", "calculate_amplitude_metric", "calculate_fitness"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
