3ef43b6cbe30e88b59463f1603e62fe2ef4a7953
Iztok Fister, Jr <iztok@iztok.dev>
2025-04-18 11:09:15 +0200

    Provide visualization of association rules (based on NarmViz)

--- niaarmts/__init__.py
+++ niaarmts/__init__.py
@@ -3,7 +3,8 @@ from niaarmts.feature import Feature
 from niaarmts.rule import build_rule
 from niaarmts.NiaARMTS import NiaARMTS
 from niaarmts.metrics import calculate_support, calculate_confidence, calculate_inclusion_metric, calculate_amplitude_metric, calculate_fitness
+from niaarmts.visualization import NarmViz
 
-__all__ = ["Dataset", "Feature", "build_rule", "NiaARMTS", "calculate_support", "calculate_confidence", "calculate_inclusion_metric", "calculate_amplitude_metric", "calculate_fitness"]
+__all__ = ["Dataset", "Feature", "build_rule", "NiaARMTS", "calculate_support", "calculate_confidence", "calculate_inclusion_metric", "calculate_amplitude_metric", "calculate_fitness", "NarmViz"]
 
 __version__ = "0.1.5"
